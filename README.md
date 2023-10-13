2201968154e4bfa12f45262afb6097e0ad471dab
Himura Kazuto <glagol15@gmail.com>
2023-10-14 00:47:24 +0400

    import

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+curl -LSs https://raw.githubusercontent.com/Himura2la/porkbun-ssl/main/install.sh | sudo bash
+```
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+hash curl || exit 1
+
+script_path="/opt/porkbun-ssl"
+systemd_path="/etc/systemd/system"
+remote_path="https://raw.githubusercontent.com/Himura2la/porkbun-ssl/main"
+
+set -x
+mkdir -p "$script_path"
+
+curl -LSs "$remote_path/retrieve.sh" --output "$script_path/retrieve.sh"
+chmod +x "$script_path/retrieve.sh"
+
+[ ! -f "$script_path/.env" ] && \
+    curl -LSs "$remote_path/.env" --output "$script_path/.env"
+
+curl -LSs "$remote_path/porkbun-ssl-retrieve.service" --output "$systemd_path/porkbun-ssl-retrieve.service"
+curl -LSs "$remote_path/porkbun-ssl-retrieve.timer" --output "$systemd_path/porkbun-ssl-retrieve.timer"
+
+systemctl daemon-reload
+set +x
+
+echo "
+TODO:
+    vim $script_path/.env
+    $script_path/retrieve.sh
+    service porkbun-ssl-retrieve start
+    service porkbun-ssl-retrieve status
+    systemctl enable porkbun-ssl-retrieve.timer
+"
